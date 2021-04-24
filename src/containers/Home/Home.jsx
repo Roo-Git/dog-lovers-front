40f4b417a06774770d7b551b79c57b13b9446b1f
Roo-Git <robert.martinez.delafuente@gmail.com>
2021-04-24 18:38:28 +0200

    Added hook and function to go to register view

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
+import { useHistory } from 'react-router'
 import Button from '../../components/Button/Button'
 
 
 function Home(props) {
+
+  // HOOKS
+  let history = useHistory();
+
+
+  // FUNCTIONS
+
+  let goToRegister = () => {
+    setTimeout(() =>{
+        history.push('/register')
+    },1000);
+  };
+
   return (
     <div className="homeContainer">
       <p>Vista Home</p>
-      <div className="buttonRegister">
+      <div className="buttonRegister" onClick={() => goToRegister()}>
       <Button name="Registro"/> 
       </div>
       <div className="buttonLogin">
