af970fdb209cb62a1c7280209076157351455cdc
Roo-Git <robert.martinez.delafuente@gmail.com>
2021-04-24 20:36:20 +0200

    Added environment variables to connect to the database

---
+++ src/api/ApiSQL.jsx
@@ -0,0 +1,6 @@
+export const port = 'http://localhost:3000';
+export const user = '/users';
+export const login = '/login';
+export const query = '?query=';
+export const logout = '/logout';
+export const update = '/update';
