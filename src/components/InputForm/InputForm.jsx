ab597602697af53a548060d5125dcb7266b5ac28
Roo-Git <robert.martinez.delafuente@gmail.com>
2021-04-24 20:14:23 +0200

    Created input component

---
+++ src/components/InputForm/InputForm.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function InputForm(props) {
+  return (
+    <div className="inputComponent">
+      <input 
+        className="inputText"
+        type={props.type}
+        name={props.name}
+        maxLength={props.lenght}
+        onChange={props.onChange}
+        value={props.value}
+        required
+      ></input>
+      <span className="floating-label" >{props.title}</span>  
+    </div>
+  )
+}
+
+export default InputForm
--- src/containers/Register/Register.jsx
+++ src/containers/Register/Register.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import InputForm from '../../components/InputForm/InputForm';
+import Button from '../../components/Button/Button';
 
 function Register(props) {
   return (
     <div className="registerContainer">
       <p>Vista Register</p>
+      <div className="inputFormMaster">
+          <InputForm 
+          type='text'
+          title="Email"
+          />
+          <InputForm 
+          type='text'
+          title="Password"
+          />
+          <div className="sendData">
+          <Button name="Enviar"/>
+          </div>
+      </div>
     </div>
   )
 }
