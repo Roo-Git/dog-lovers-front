0af82f3b3c09f772dc82e3e220c517a0a0362d6f
Roo-Git <robert.martinez.delafuente@gmail.com>
2021-04-24 14:37:19 +0200

    Created button component

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import Button from '../../components/Button/Button'
+
 
 function Home(props) {
   return (
     <div className="homeContainer">
-      <p>I'am Home!</p>
+      <p>Vista Home</p>
+      <div className="buttonRegister">
+      <Button name="Registro"/> 
+      </div>
+      <div className="buttonLogin">
+      <Button name="Login"/> 
+      </div>
     </div>
   )
 }
