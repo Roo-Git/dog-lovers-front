c19618b56755123d14fe893ba697cf5a1a28a17d
Roo-Git <robert.martinez.delafuente@gmail.com>
2021-04-27 18:57:05 +0200

    Added new section design to home view

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 import Header from '../../components/Header/Header';
 import Navbar from '../../components/Navbar/Navbar';
 import dogsHome from '../../img/dogsHome.png';
+import loverDog from '../../img/loverDog.png';
 
+
+// <h1>Cause we love time, we love dogs.</h1>
 
 function Home(props) {
 
@@ -28,21 +31,26 @@ function Home(props) {
                 <h1>Do you want to take care of your neighbor's dog?</h1>
                 <h1>Or do you prefer that they take your dog for a walk?</h1>                
               </div>
-              
+
               <img className="dogsHome" src={dogsHome} alt="dogsHome"/>
 
           </div>
 
-          <h2>Dog lovers is your corner to make new friends, new pals.</h2>
-          <h2>While you finish work, go on vacation, or walk someone else's dog.</h2>
-          <h2>Cause we love time, we love dogs.</h2>
+          <div className="h2HomeMaster">
+
+              <div className="h2Home">
+                <h2>Dog lovers is your corner to make new friends, new pals.</h2>
+                <h2>While you finish work, go on vacation, or walk someone else's dog.</h2>
+              </div>
+
+              <img className="loverDog" src={loverDog} alt="LoverDog"/>
+              
+          </div>
+
         </div>
-        
-        
 
       </div>
-
-      
+  
     </div>
   )
 }
