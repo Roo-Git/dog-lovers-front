65b689aabfae9b9c512a573a8a24fe79cc33daad
Roo-Git <robert.martinez.delafuente@gmail.com>
2021-05-01 10:23:32 +0200

    Created component to create a dog

---
+++ src/components/DogForm/DogForm.jsx
@@ -0,0 +1,126 @@
+import React, { useState} from 'react'
+import axios from 'axios';
+import {connect} from 'react-redux';
+import InputForm from "../../components/InputForm/InputForm";
+import PersonalButton from "../../components/PersonalButton/PersonalButton";
+import {port, pet} from "../../api/ApiSQL";
+
+
+function DogForm(props) {
+
+  const [dog, setDog] = useState ({
+    user_Id: '',
+    name : '',
+    gender: '',
+    breed: '',
+    size: '',
+    age: '',
+    description: '',
+    
+  });
+
+  const handleState = (ev) => {
+    setDog ({...dog, [ev.target.name]: ev.target.value})
+  }
+
+  const toggle = async (ev) => {
+
+    ev.preventDefault()
+
+    let body = {
+      user_Id: props.user.id,
+      name: dog.name,
+      gender: dog.gender,
+      breed: dog.breed,
+      size: dog.size,
+      age: dog.age,
+      description: dog.description
+    };
+
+    console.log(body, "======BODY")
+
+    try {
+      let id = props.user.id
+      let result = await axios.post(`${port}${pet}/${id}`, body)
+      console.log(result, 'Perro creado con exito')
+
+    }catch (error){
+      console.log(error, 'El perr@ no ha podido ser creado')
+
+    }
+  };
+
+  return (
+    <div className="dogFormComponent">
+      <form className="registerForm" onSubmit={toggle}>
+            <h2>Dog Registration</h2>
+            <div className="input">
+              <InputForm 
+                type="input"
+                title="Name"
+                name="name"
+                onChange={handleState}
+                value={dog.name}
+              />
+            </div>
+            <div className="input">
+              <InputForm 
+                type="input"
+                title="Gender"
+                name="gender"
+                onChange={handleState}
+                value={dog.gender}
+              />
+            </div>
+            <div className="input">
+              <InputForm 
+                type="input"
+                title="Breed"
+                name="breed"
+                onChange={handleState}
+                value={dog.breed}
+              />
+            </div>
+            <div className="input">
+              <InputForm 
+                type="tinput"
+                title="Size"
+                name="size"
+                onChange={handleState}
+                value={dog.size}
+              />
+            </div>
+            <div className="input">
+              <InputForm 
+                type="input"
+                title="Age"
+                name="age"
+                onChange={handleState}
+                value={dog.age}
+              />
+            </div>
+            <div className="input">
+              <InputForm 
+                type="input"
+                title="Description"
+                name="description"
+                onChange={handleState}
+                value={dog.description}
+              />
+            </div>
+            <div className="submit">
+              <PersonalButton name="Submit"/> 
+            </div>
+            <div className="closeForm"></div>
+        </form>
+    </div>
+  )
+}
+
+const mapStateToProps = state => {
+  return {
+      user : state.userReducer.user,
+  }
+}
+
+export default connect(mapStateToProps)(DogForm);
--- src/containers/Owner/Owner.jsx
+++ src/containers/Owner/Owner.jsx
@@ -1,123 +1,33 @@
-import axios from 'axios';
-import React, { useState} from 'react'
+import React from 'react'
 import {connect} from 'react-redux';
+import DogForm from '../../components/DogForm/DogForm';
 import Header from "../../components/Header/Header";
 import Navbar from "../../components/Navbar/Navbar";
-import InputForm from "../../components/InputForm/InputForm";
-import PersonalButton from "../../components/PersonalButton/PersonalButton";
-import {port, pet} from "../../api/ApiSQL";
+
+
+
 
 function Owner(props) {
 
-  const [dog, setDog] = useState ({
-    user_Id: '',
-    name : '',
-    gender: '',
-    breed: '',
-    size: '',
-    age: '',
-    description: '',
-    
-  });
-  console.log(props, "Que nos traen las props?")
 
-  const handleState = (ev) => {
-    setDog ({...dog, [ev.target.name]: ev.target.value})
-  }
-
-  const toggle = async (ev) => {
-
-    ev.preventDefault()
-
-    let body = {
-      user_Id: props.user.id,
-      name: dog.name,
-      gender: dog.gender,
-      breed: dog.breed,
-      size: dog.size,
-      age: dog.age,
-      description: dog.description
-    };
-
-    console.log(body, "======BODY")
-
-    try {
-      let id = props.user.id
-      let result = await axios.post(`${port}${pet}/${id}`, body)
-      console.log(result, 'Perro creado con exito')
-
-    }catch (error){
-      console.log(error, 'El perr@ no ha podido ser creado')
-
-    }
-  }
+  
   return (
     <div className="ownerContainer">
-      <Header/>
-      <Navbar/>
-      <form className="registerForm" onSubmit={toggle}>
-            <h2>Dog Registration</h2>
-            <div className="input">
-              <InputForm 
-                type="input"
-                title="Name"
-                name="name"
-                onChange={handleState}
-                value={dog.name}
-              />
-            </div>
-            <div className="input">
-              <InputForm 
-                type="input"
-                title="Gender"
-                name="gender"
-                onChange={handleState}
-                value={dog.gender}
-              />
-            </div>
-            <div className="input">
-              <InputForm 
-                type="input"
-                title="Breed"
-                name="breed"
-                onChange={handleState}
-                value={dog.breed}
-              />
-            </div>
-            <div className="input">
-              <InputForm 
-                type="tinput"
-                title="Size"
-                name="size"
-                onChange={handleState}
-                value={dog.size}
-              />
-            </div>
-            <div className="input">
-              <InputForm 
-                type="input"
-                title="Age"
-                name="age"
-                onChange={handleState}
-                value={dog.age}
-              />
-            </div>
-            <div className="input">
-              <InputForm 
-                type="input"
-                title="Description"
-                name="description"
-                onChange={handleState}
-                value={dog.description}
-              />
-            </div>
-            <div className="submit">
-              <PersonalButton name="Submit"/> 
-            </div>
-            <div className="closeForm"></div>
-        </form>
-      
-      
+      {
+        !props.user.id
+        ?
+        <>
+        <Header/>
+        <Navbar/>
+        
+        </>
+        :
+        <>
+        <Header/>
+        <Navbar/>
+        <DogForm/>
+        </>
+      }
     </div>
   )
 }
