b0a16735495e4db0e61547d2c059a27aab875354
Roo-Git <robert.martinez.delafuente@gmail.com>
2021-05-02 20:35:43 +0200

    Added call to get all request

--- src/containers/Sitter/Sitter.jsx
+++ src/containers/Sitter/Sitter.jsx
@@ -1,15 +1,45 @@
-import React from 'react'
+import axios from 'axios';
+import React, {useState}from 'react'
 import Header from '../../components/Header/Header'
 import Navbar from '../../components/Navbar/Navbar'
+import {connect} from 'react-redux';
+import { careRequest, port } from '../../api/ApiSQL';
 
-function Sitter() {
+
+function Sitter(props) {
+  const [request, setRequest] = useState()
+
+  const handleState = (ev) => {
+    setRequest ({...request, [ev.target.name]: ev.target.value})
+  };
+  const toggle = async (ev) => {
+    ev.preventDefault()
+
+    try{
+      let result = await axios.get(`${port}${careRequest}`)
+      console.log(result, "Requests obtenidas con exito")
+      if(result){
+      }
+    } catch(error) {
+      console.log(error, 'Las request no han podido ser obtenidas')
+    }
+ };
   return (
     <div className="sitterComponent">
       <Header/>
       <Navbar/> 
       <p>Vista Sitter</p>
+      <button onChange={handleState} onClick={toggle}></button>
     </div>
   )
 }
 
-export default Sitter
+const mapStateToProps = state => {
+  return {
+    user : state.userReducer.user,
+    dog  : state.dogReducer.dog,
+    request : state.requestReducer.request
+  }
+}
+
+export default connect(mapStateToProps)(Sitter);
