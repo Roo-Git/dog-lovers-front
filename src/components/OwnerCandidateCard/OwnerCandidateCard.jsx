ce18782bcaf78a3b3ebdbe4424cccbbb82e98fa0
Roo-Git <robert.martinez.delafuente@gmail.com>
2021-05-07 17:59:43 +0200

    Trying to get a candidate related to user owner

--- src/components/OwnerCandidateCard/OwnerCandidateCard.jsx
+++ src/components/OwnerCandidateCard/OwnerCandidateCard.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import axios from 'axios';
+import React, {useState, useEffect} from 'react'
 import {connect} from 'react-redux';
+import { candidates, port } from '../../api/ApiSQL';
 
 function OwnerCandidateCard(props) {
+
+  const [candidate, setCandidate] = useState({
+    myCandidate: [{}],
+  });
+
+  useEffect(()=>{
+    getCandidate();
+  },[]); // eslint-disable-line react-hooks/exhaustive-deps
+
+  // Function GET CANDIDATE BY ID (Necesito el ID de la care request)
+
+  // Ahora mismo estoy hardcodeando la ID del candidato.
+
+  const getCandidate = async (props) => {
+
+    try{
+      let id = 1
+      let result = await axios.get(`${port}${candidates}/${id}`)
+      setCandidate({...candidate, myCandidate: result.data})
+      if(result){
+        console.log(result, "Candidato obtenido con exito.")
+      }
+    } catch(error) {
+      console.log(error, "No se ha podido obtener el candidato con exito")
+    }
+  }
+
   return (
     <div className="ownerCandidateCard">
+
+    </div>
+  )
+}
+
+const mapStateToProps = state => {
+  return {
+      user : state.userReducer.user,
+      candidate  : state.candidateReducer.candidate
+  }
+}
+
+export default connect(mapStateToProps)(OwnerCandidateCard);
+
+
+
+
+/*
       {
         props.candidate.length > 0
         ?
@@ -26,14 +73,7 @@ function OwnerCandidateCard(props) {
         <h2 className="not-candidate">Still don't have candidate.</h2>
         </>
       }
-    </div>
-  )
-}
 
-const mapStateToProps = state => {
-  return {
-      candidate  : state.candidateReducer.candidate
-  }
-}
 
-export default connect(mapStateToProps)(OwnerCandidateCard);
+
+*/
