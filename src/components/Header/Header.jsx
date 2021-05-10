81706e507f68aa1537646ac85782d2a093ac3ebb
Roo-Git <robert.martinez.delafuente@gmail.com>
2021-05-10 12:24:04 +0200

    Refactored header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import dogHeader from '../../img/dogHeader.png';
+import dogHeader from '../../img/dogHeader.png'
 import { useHistory } from 'react-router';
 
 function Header(props) {
@@ -18,7 +18,8 @@ function Header(props) {
   
   return (
     <div className="headerComponent">
-      <img className="dogHeader" src={dogHeader} alt="dogHeader" onClick={goToBeginning}/>
+      <div className="space-header"></div>
+      <img className="dog-header" src={dogHeader} alt="dog-header" onClick={goToBeginning}/>
       <div className="title">Dog Lovers</div>
     </div>
   )
