7e863d251c89ae46ce6b3e973c94f1b94dbc3049
Roo-Git <robert.martinez.delafuente@gmail.com>
2021-05-11 11:14:17 +0200

    Resolved issue with a img

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import dogHeader from '../../img/dogHeader.png'
+import DogHeader from '../../img/DogHeader.png'
 import { useHistory } from 'react-router';
 
 function Header(props) {
@@ -19,7 +19,7 @@ function Header(props) {
   return (
     <div className="headerComponent">
       <div className="space-header"></div>
-      <img className="dog-header" src={dogHeader} alt="dog-header" onClick={goToBeginning}/>
+      <img className="dog-header" src={DogHeader} alt="dog-header" onClick={goToBeginning}/>
       <h1 className="title">Dog Lovers</h1>
     </div>
   )
